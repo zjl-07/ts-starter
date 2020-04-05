4a4ab3ec78917c0064eae11ebde383b1030f0397
EmildaZhang <emildazhg07@gmail.com>
2020-04-05 13:48:46 +0700

    refactoring logic for counting winner between age 20-30

--- statistic-analytic/src/index.ts
+++ statistic-analytic/src/index.ts
@@ -12,9 +12,8 @@ const data: string[][] = fs
 // 1 Analys : winner from age 20-30
 let winCount = 0;
 data.forEach((colomn) => {
-  if (parseInt(colomn[2]) > 20 && parseInt(colomn[2]) < 30) {
-    winCount++;
-  }
+  let age = parseInt(colomn[2]);
+  age > 20 && age < 30 && winCount++;
 });
 
 console.log(`Total winner age range from 20-30 : ${winCount} people`);
