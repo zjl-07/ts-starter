d8728476b9abe5dfcdc80a2fadad889fc3e82573
EmildaZhang <emildazhg07@gmail.com>
2020-04-05 13:36:29 +0700

    analyst some data and report it to console.log

--- statistic-analytic/src/index.ts
+++ statistic-analytic/src/index.ts
@@ -1,13 +1,20 @@
 import fs from "fs";
 
-//async file reading
-const data = fs
+const data: string[][] = fs
   .readFileSync("oscar-female-winner.csv", {
     encoding: "utf-8"
   })
   .split("\n")
-  .map((row) => {
+  .map((row: string): string[] => {
     return row.split(",");
   });
 
-console.log(data); 
+// 1 Analys : winner from age 20-30
+let winCount = 0;
+data.forEach((colomn) => {
+  if (parseInt(colomn[2]) > 20 && parseInt(colomn[2]) < 30) {
+    winCount++;
+  }
+});
+
+console.log(`Total winner age range from 20-30 : ${winCount} people`);
